bc7c27a4cca3cdd0122fe04fbb9e3e133bf1d445
matthew <permahuman@gmail.com>
2022-12-09 13:38:51 -0800

    First skeleton files and funcitons in js file for Library App.

---
+++ script.js
@@ -0,0 +1,42 @@
+myBooks = []
+
+function Book(title,author,pages,readYet,rating) {
+
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.readYet = readYet
+    this.rating = rating
+    //constructor
+}
+
+function addBook() {
+
+    //add book to library
+}
+
+function newBook(){
+
+    //function that takes use rinput and adds a new book based on their input
+}
+
+/*Write a function that loops through the array and displays each book on the page. 
+You can display them in some sort of table, or each on their own “card”. 
+It might help for now to manually add a few books to your array so you can see the display.
+
+Add a “NEW BOOK” button that brings up a form allowing users to input the details for 
+the new book: author, title, number of pages, whether it’s been read 
+and anything else you might want.
+
+
+You will most likely encounter an issue where submitting your form will not do what you expect it to do. 
+That’s because the submit input tries to send the data to a server by default. 
+Read up on the event.preventDefault documentation again and see how 
+Add a button on each book’s display to remove the book from the library.
+
+You will need to associate your DOM elements with the actual book objects in some way. 
+
+One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+
+Add a button on each book’s display to change its read status.
+To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.*/
